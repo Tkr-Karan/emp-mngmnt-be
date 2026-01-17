7be4bcc0bbc0ea094353349fbd15eae4909fb051
Karan Kumar <karan@probus.io>
2026-01-17 16:41:00 +0530

    add: cors check

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -18,11 +18,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'employees',  # Your App [cite: 50]
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,3 +105,33 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # Local development
+    "http://localhost:3000",
+    "https://your-frontend-domain.com",  # Production frontend domain
+    "https://emp-mngmnt-be-production.up.railway.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
